4d14235cc6d417f1b041af06ffbcf29667e77dd5
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-03-04 16:17:53 +0800

    normal update
    add qiniu but may not work.

---
+++ qiniu/index.js
@@ -0,0 +1,33 @@
+var qiniu = require("qiniu");
+
+qiniu.conf.ACCESS_KEY = 'DqvJP-ITkUwXFFwo70JjOY5QAFttOQGzzUMVzuED';
+qiniu.conf.SECRET_KEY = 'n82Vc2kCzFBtde2qL9eYFjFI8JJL788_BDhP1vzP';
+//要上传的空间
+bucket = 'from-wechat';
+//上传到七牛后保存的文件名
+key = 'my-nodejs-test.txt';
+
+//构建上传策略函数
+function uptoken(bucket, key) {
+  var putPolicy = new qiniu.rs.PutPolicy(bucket+":"+key);
+  return putPolicy.token();
+}
+//生成上传 Token
+token = uptoken(bucket, key);
+//要上传文件的本地路径
+filePath = 'http://ppe.oss-cn-shenzhen.aliyuncs.com/collections/36/4/thumb.jpg';
+//构造上传函数
+function uploadFile(uptoken, key, localFile) {
+  var extra = new qiniu.io.PutExtra();
+    qiniu.io.putFile(uptoken, key, localFile, extra, function(err, ret) {
+      if(!err) {
+        // 上传成功， 处理返回值
+        console.log(ret.hash, ret.key, ret.persistentId);       
+      } else {
+        // 上传失败， 处理返回代码
+        console.log(err);
+      }
+  });
+}
+//调用uploadFile上传
+uploadFile(token, key, filePath);
